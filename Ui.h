0f13c3447dafa733cb19fb8c35cbfaae5194e6b3
NicoletaAlexandraBodasca <64136441+NicoletaAlexandraBodasca@users.noreply.github.com>
2020-04-22 15:28:28 +0300

    Add files via upload

---
+++ Ui.h
@@ -0,0 +1,29 @@
+#pragma once
+#include"Controller.h"
+#include<string>
+class Ui
+{
+private:
+	Controller cnt;
+
+public:
+	Ui();
+
+	Ui(Controller cnt);
+
+	Ui(const Ui & other);
+
+	Ui& operator=(const Ui & other) {
+		this->cnt = other.cnt;
+		return *this;
+	}
+
+	void Start();
+
+	void printMenu();
+
+	void addClient();
+
+	~Ui();
+};
+
